76c175d0454212a1191d41e7eb581e78be50b89b
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-22 14:58:02 -0400

    Update ComputationalEcology.rb

--- ComputationalEcology.rb
+++ ComputationalEcology.rb
@@ -1,7 +1,7 @@
-//let formsA: [&str; 16] = ["Deep Sea", "Lakes", "Rivers", "Coasts", "Wetlands", "Desert", "Plains", "Forest", "Caves", "Tundra", "Valley", "Mountains", "Rock Formations", "Weather", "Atmospheric Optical Phenomenona", "Astronomy"];
-//let formsB: [f32; 16] = [5.9, 6.5, 7.1, 7.7, 4.1, 2.9, 8.9, 9.5, 4.7, 2.3, 8.3, 5.3, 3.5, 1.7, 0.5, 1.1];
-//let successionsA: [&str; 9] = ["fires", "floods", "droughts", "windstorms", "landslides", "avalanches", "volcanic eruptions", "disease epidemics", "ocean temperature changes"];
-//let successionsB: [f32; 9] = [2.0, 5.0, 3.0, 6.0, 7.0, 9.0, 1.0, 8.0, 4.0];
+#formsA = ["Deep Sea", "Lakes", "Rivers", "Coasts", "Wetlands", "Desert", "Plains", "Forest", "Caves", "Tundra", "Valley", "Mountains", "Rock Formations", "Weather", "Atmospheric Optical Phenomenona", "Astronomy"];
+#formsB = [f32; 16] = [5.9, 6.5, 7.1, 7.7, 4.1, 2.9, 8.9, 9.5, 4.7, 2.3, 8.3, 5.3, 3.5, 1.7, 0.5, 1.1];
+#successionsA = [&str; 9] = ["fires", "floods", "droughts", "windstorms", "landslides", "avalanches", "volcanic eruptions", "disease epidemics", "ocean temperature changes"];
+#successionsB = [f32; 9] = [2.0, 5.0, 3.0, 6.0, 7.0, 9.0, 1.0, 8.0, 4.0];
 
 
 puts "Deep Sea"
